52fc6f1c304fb6db898def55673588803cbb32b8
chence <leoooon@foxmail.com>
2025-08-13 16:57:46 +0800

    cc

--- src/web/views/index.js
+++ src/web/views/index.js
@@ -1,7 +1,5 @@
 createApp({
-    data: {
-
-    },
+    data: {},
     methods: {
         init: async function () {
             let self = this;
@@ -12,5 +10,22 @@ createApp({
         initListeners: function () {
             let self = this;
         },
+        downloadDiv: function () {
+            // 获取目标div元素
+            const targetDiv = document.getElementById('testId');
+            // 使用dom-to-image转换为PNG
+            domtoimage.toPng(targetDiv)
+                .then(function (dataUrl) {
+                    const link = document.createElement('a');
+                    link.download = 'dom-screenshot.png';
+                    link.href = dataUrl;
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                })
+                .catch(function (error) {
+                    console.error('转换失败:', error);
+                });
+        }
     }
 });
